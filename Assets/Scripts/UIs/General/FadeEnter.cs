9a1fcc5bf27e502c8c39fdb6117d971faed11d4d
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-06-03 07:50:07 +0700

    feat: fade enter device manager ui

---
+++ Assets/Scripts/UIs/General/FadeEnter.cs
@@ -0,0 +1,28 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeEnter : MonoBehaviour
+{
+    [SerializeField] private Vector2 _startOffset;
+    [SerializeField] private float _duration = 0.5f;
+    private Image _image;
+    private CanvasGroup _canvasGroup;
+
+
+    private void Awake() {
+        _image = GetComponent<Image>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+
+    private void Start() {
+        Vector2 targetPosition = _image.rectTransform.anchoredPosition;
+        _image.rectTransform.anchoredPosition = targetPosition + _startOffset;
+        _canvasGroup.alpha = 0;
+
+
+        _image.rectTransform.DOAnchorPos(targetPosition, _duration).SetEase(Ease.OutSine);
+        _canvasGroup.DOFade(1, _duration).SetEase(Ease.OutSine);
+    }
+}
