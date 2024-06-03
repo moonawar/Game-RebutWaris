51343f16a5031c4434c2b9f986da7c62e9a6c266
Edia Zaki <13521141@mahasiswa.itb.ac.id>
2024-06-03 17:12:30 +0700

    feat: tutorial functions

---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+    private int currentTutorial = 1;
+    private bool _active = false;
+    [SerializeField] private Button LeftButton;
+    [SerializeField] private Button RightButton;
+    [SerializeField] private List<GameObject> Tutorials;
+
+    public void Activate()
+    {
+        _active = true;
+        currentTutorial = 1;
+        ChangeTutorial();
+    }
+
+    public void Deactivate()
+    {
+        _active = false;
+    }
+
+    private void ChangeTutorial()
+    {
+        for(int i = 0; i < Tutorials.Count; i++)
+        {
+            if(i == currentTutorial - 1)
+            {
+                Tutorials[i].SetActive(true);
+            }
+            else
+            {
+                Tutorials[i].SetActive(false);
+            }
+        }
+
+        if(currentTutorial >= 7)
+        {
+            RightButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            RightButton.gameObject.SetActive(true);
+        }
+
+        if (currentTutorial <= 1)
+        {
+            LeftButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            LeftButton.gameObject.SetActive(true);
+        }
+    }
+
+    public void NextTutorial()
+    {
+        print(_active);
+        if (!_active) return;
+
+        currentTutorial++;
+        ChangeTutorial();      
+    }
+
+    public void PreviousTutorial()
+    {
+        if (!_active) return;
+
+        currentTutorial--;
+        ChangeTutorial(); 
+    }
+}
